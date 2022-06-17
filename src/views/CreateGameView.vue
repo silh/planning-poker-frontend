795cb2fd16ba5718b9dac6bf16fe416c672b1171
Ilia Beliaev <i.beliaev@activevideo.com>
2022-06-17 23:35:02 +0200

    First views - create game, main screen, list games. TODO routes.

---
+++ src/views/CreateGameView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import http from "../http-commons.js";
+import { ref } from "vue";
+
+const gameName = ref("");
+
+const emit = defineEmits("created");
+
+async function send() {
+  await http.post("/games", {
+    gameName: gameName.value,
+    creator: {
+      id: 11, // TODO this is a placeholder for real request.
+      name: "aName",
+    },
+  });
+  emit("created");
+}
+</script>
+
+<template>
+  <main>
+    <div>Start a new game.</div>
+    <label id="game-name">Game name:</label>
+    <input v-model="gameName" id="game-name" />
+    <button @click="send">Create a game</button>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/ListGamesView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import http from "../http-commons.js";
+
+const games = ref([]);
+
+const fetchGames = async () => {
+  let resp = await http.get("/games");
+  games.value = resp.data;
+};
+
+onMounted(fetchGames);
+</script>
+
+<template>
+  <div>
+    <div v-for="game in games" :key="game">{{ game }}</div>
+  </div>
+</template>
+
+<style scoped></style>
